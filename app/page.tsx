9f6df51e90adad138bd8e553aa506fb4ec724b15
arshpreetEnsuredit <arshpreet@ensuredit.com>
2026-01-20 12:23:08 +0530

    Fix root page redirect using client-side navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from 'next/navigation'
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  redirect('/welcome', 'replace')
+  const router = useRouter()
+  
+  useEffect(() => {
+    router.replace('/welcome')
+  }, [router])
+  
+  return null
 }
